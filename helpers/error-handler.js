8c5b9ff6720838514bc59fbe8a9a7a71c639c329
itaytzdaka <itaytzdaka1@gmail.com>
2022-05-08 01:07:01 +0300

    first commit

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -1,7 +1,8 @@
 function getError(err) {
 
     if (process.env.NODE_ENV === "production") {
-        return "Some error occurred, please try again later.";
+        return err.message;
+        //return "Some error occurred, please try again later.";
     }
 
     return err.message;
